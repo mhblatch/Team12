5997b88f0697e28736fa81ddc7f184b9a321d34b
Hugo Burbelo <hburbelo@phone2action.com>
2022-01-08 15:32:17 -0500

    Complete ghost in range

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -4,7 +4,35 @@ import java.io.*;
 
 public class TestGhostInRange extends TestCase {
 
-	public void testGhostInRange() throws FileNotFoundException{
+	public void testGhostInRangeTrue() throws FileNotFoundException{
+		//Creating A Map
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
+
+		Ghost ghost = frame.addGhost(new Location(1, 2), "test", Color.BLUE);
+
+		//Start The Game
+		frame.startGame();
+
+		assertTrue(ghost.is_pacman_in_range()); // check we have moved to a valid location
+
+	}
+
+	public void testGhostInRangeFalse() throws FileNotFoundException{
+		//Creating A Map
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
+
+		Ghost ghost = frame.addGhost(new Location(3, 4), "test", Color.BLUE);
+
+		//Start The Game
+		frame.startGame();
+
+		assertFalse(ghost.is_pacman_in_range()); // check we have moved to a valid location
 
 	}
 }
