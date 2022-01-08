af84492515b902077b22d860b5c2346aa8601871
Tim_Seidell <seidell.tim@gmail.com>
2022-01-07 20:43:07 -0500

    Implemented pacman-get_valid_moves()

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -6,7 +6,7 @@ public class PacMan{
 	String myName;
 	Location myLoc;
 	Map myMap;
-	Location shift; 
+	Location shift;
 
 	public PacMan(String name, Location loc, Map map) {
 		this.myLoc = loc;
@@ -15,7 +15,32 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		int x = myLoc.x;
+		int y = myLoc.y;
+
+		ArrayList<Location> locations = new ArrayList<Location>();
+
+		// Check move up
+		if(!myMap.getLoc(new Location(x, y + 1)).contains(Map.Type.WALL)) {
+			locations.add(new Location(x, y + 1));
+		}
+
+		// Check move down
+		if(!myMap.getLoc(new Location(x, y - 1)).contains(Map.Type.WALL)) {
+			locations.add(new Location(x, y - 1));
+		}
+
+		// Check move left
+		if(!myMap.getLoc(new Location(x - 1, y)).contains(Map.Type.WALL)) {
+			locations.add(new Location(x - 1, y));
+		}
+
+		// Check move right
+		if(!myMap.getLoc(new Location(x + 1, y)).contains(Map.Type.WALL)) {
+			locations.add(new Location(x + 1, y));
+		}
+
+		return locations;	
 	}
 
 	public boolean move() {
