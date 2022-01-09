2ec79c6a4719716a72e2c8fd823b5d01bd16e5e0
Tim-Seidell <46984533+Tim-Seidell@users.noreply.github.com>
2022-01-09 16:23:54 -0500

    Added Test: pacman-get_valid_moves

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,10 +1,23 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+		// Creates A New Map With Walls and Tokens Initialized
+		MainFrame frame = new MainFrame();
+
+		// Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		
+		// Created expected result
+		ArrayList<Location> expected = new ArrayList<Location>();
+		expected.add(new Location(1, 2));		// Up
+		expected.add(new Location(2, 1)); 		// Right
+
+		// Test Expected result
+		assertEquals(expected, pacman.get_valid_moves());
 	}
 }
